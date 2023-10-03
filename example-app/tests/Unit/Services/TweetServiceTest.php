f3295993a210b7dd03483666254a2316d1dfd255
munablamu <coffee.git@gmail.com>
2023-10-03 16:55:34 +0900

    ch5.3 「デプロイを実行する」

--- example-app/tests/Unit/Services/TweetServiceTest.php
+++ example-app/tests/Unit/Services/TweetServiceTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit\Services;
 use PHPUnit\Framework\TestCase;
 use App\Services\TweetService;
 use Mockery;
+use App\Modules\ImageUpload\ImageManagerInterface;
 
 class TweetServiceTest extends TestCase
 {
@@ -14,13 +15,14 @@ class TweetServiceTest extends TestCase
      */
     public function test_check_own_tweet(): void
     {
-        $tweetService = new TweetService();
-
         $mock = Mockery::mock('alias:App\Models\Tweet');
         $mock->shouldReceive('where->first')->andReturn((object)[
             'id' => 1,
             'user_id' => 1
         ]);
+
+        $imageManager = Mockery::mock(ImageManagerInterFace::class);
+        $tweetService = new TweetService($imageManager);
 
         $result = $tweetService->checkOwnTweet(1, 1);
         $this->assertTrue($result);
