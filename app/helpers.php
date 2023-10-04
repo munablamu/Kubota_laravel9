8ab3bc3c4ee323cf51799ca366b36bf366f3feed
munablamu <coffee.git@gmail.com>
2023-10-04 14:01:39 +0900

    誤字修正

--- app/helpers.php
+++ app/helpers.php
@@ -5,7 +5,7 @@ if ( !function_exists('image_url') ) {
     function image_url(string $path): string
     {
         if ( app()->environment('production') ) {
-            return (string) app()->make(\Cloudinary\Croudinary::class)->image($path)->secure();
+            return (string) app()->make(\Cloudinary\Cloudinary::class)->image($path)->secure();
         }
         return asset('storage/images/' . $path);
     }
